48243744ea6a2b77e320526db63ff5e0ecebbc27
Manu Kakkar <manu2551988@gmail.com>
2020-07-08 08:10:11 +0530

    Push

--- src/main/java/com/tyss/project/lib/BaseLib.java
+++ src/main/java/com/tyss/project/lib/BaseLib.java
@@ -10,12 +10,14 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public class BaseLib {
 	static
 	{
 		System.setProperty(GenericLib.getPropData("BrowserKey"), GenericLib.getPropData("BrowserPath"));
 	}
 	public WebDriver driver;
+	
 	@BeforeMethod
 	public void setUp()
 	{
