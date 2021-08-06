87de7dd04803b4e31ff7cb927f651c448e6b13cb
Manu Kakkar <manu2551988@gmail.com>
2021-08-06 20:12:43 +0530

    Update

---
+++ src/test/java/parameters/Demo_Parameters.java
@@ -0,0 +1,23 @@
+package parameters;
+
+import org.openqa.selenium.By;
+import org.sikuli.api.robot.Key;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.scorpiontech.project.lib.BaseTest;
+
+public class Demo_Parameters extends BaseTest {
+	
+	
+	@Test
+	@Parameters({"searchInput"})
+	public void TC_01(String searchInput) {
+
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys(searchInput,Key.ENTER);
+		
+	}
+
+}
