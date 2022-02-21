d302d6ce72f5031540a6c7267968a791959d9f1b
Manu Kakkar <manukakk@amazon.com>
2022-02-22 01:40:35 +0530

    AFW v 4.2 WebDriver Utilities

---
+++ src/main/java/lib/WebDriverDriverUtilies.java
@@ -0,0 +1,64 @@
+package lib;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Set;
+
+public class WebDriverDriverUtilies {
+	
+	public void dropDown(WebElement ele,String text) {
+		Select s=new Select(ele);
+		s.selectByVisibleText(text);
+		
+	}
+	
+	public void mouseHover(WebDriver driver,WebElement ele) {
+		Actions a=new Actions(driver);
+		a.moveToElement(ele).perform();
+	}
+	
+	public void rightClick(WebDriver driver,WebElement ele) {
+		Actions a=new Actions(driver);
+		a.contextClick(ele).perform();
+	}
+	
+	public void doubleClick(WebDriver driver,WebElement ele) {
+		Actions a=new Actions(driver);
+		a.doubleClick(ele).perform();
+	}
+	
+	public void dragdrop(WebDriver driver,WebElement source,WebElement target) {
+		Actions a=new Actions(driver);
+		a.dragAndDrop(source, target).perform();
+	}
+	
+	public void switchframe(WebDriver driver) {
+		driver.switchTo().frame(0);
+	}
+	
+	public void switchbackframe(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+	
+	public void alertPopup(WebDriver driver) {
+		driver.switchTo().alert().accept();
+	}
+	
+	public void switchingTabs(WebDriver driver) {
+		Set<String> child = driver.getWindowHandles();
+		for(String c:child) {
+			driver.switchTo().window(c);
+		}
+	}
+	
+	public void scrollBar(WebDriver driver,int x,int y) {
+		JavascriptExecutor js= (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy("+x+","+y+")");
+	}
+	
+
+}
