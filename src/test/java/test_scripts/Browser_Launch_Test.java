3069d2a6f9a75f74c6f42d9c06166bbdda5ac74e
Manu Kakkar <manukakk@amazon.com>
2022-02-22 01:10:11 +0530

    AFW v 4.2 WebDriver Manager

--- src/test/java/test_scripts/Browser_Launch_Test.java
+++ src/test/java/test_scripts/Browser_Launch_Test.java
@@ -1,5 +1,6 @@
 package test_scripts;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lib.BaseTest;
 import org.testng.annotations.Test;
 
@@ -8,6 +9,8 @@ public class Browser_Launch_Test extends BaseTest {
 
     @Test
     public void TC_01() {
+
         driver.get("https://www.google.com/");
+
     }
 }
